ec91252d14da59e4ed159ef405e4183c9cfbeeaa
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-16 15:36:53 +0700

    Update rent_states.py

--- states/rent_states.py
+++ states/rent_states.py
@@ -1,12 +1,16 @@
 from aiogram.fsm.state import StatesGroup, State
 
-class RentStates(StatesGroup):
-    choosing_bike = State()
-    choosing_package = State()
-    choosing_days = State()
-    choosing_payment_type = State()
-    choosing_currency = State()
-    choosing_deposit_type = State()
-    choosing_deposit_currency = State()
-    choosing_deposit_payment_type = State()
-    confirmation = State()
+class RentWizard(StatesGroup):
+    operation = State()
+    model = State()
+    package = State()
+    days = State()
+    time = State()
+    tank = State()
+    clean = State()
+    equipment = State()
+    payment_amount = State()
+    payment_method = State()
+    deposit_type = State()
+    deposit_currency = State()
+    deposit_method = State()
