4518ce369240ceb2bf8f601a959bf4cc49802491
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-16 15:13:36 +0700

    Create services/sheet_service.py

---
+++ services/sheet_service.py
@@ -0,0 +1,10 @@
+class SheetsService:
+
+    async def create_rent(self, data: dict):
+        pass
+
+    async def close_rent(self, rent_id: int):
+        pass
+
+    async def get_open_rents(self):
+        return []
