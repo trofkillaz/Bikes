7779ce8009486476f358b4fbfc72ca89592cb164
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-16 14:48:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-
 from config import BOT_TOKEN
+
 from handlers import menu, create_rent, open_requests, close_rent
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+    dp = Dispatcher()
 
     dp.include_router(menu.router)
     dp.include_router(create_rent.router)
@@ -15,6 +14,7 @@ async def main():
     dp.include_router(close_rent.router)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
