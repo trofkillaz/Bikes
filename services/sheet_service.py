3dd86d2ae29ab87b10042559a2c8ab6724c1100e
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-16 15:20:22 +0700

    Update sheet_service.py

--- services/sheet_service.py
+++ services/sheet_service.py
@@ -14,3 +14,6 @@ sheets_service = SheetsService()
 
 async def get_active_rents():
     return await sheets_service.get_open_rents()
+
+async def close_rent_by_number(rent_id: int):
+    return await sheets_service.close_rent(rent_id)
