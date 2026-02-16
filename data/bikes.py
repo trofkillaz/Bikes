e4d5bdbc58251ccb3d025ceabb291b67ce88b61f
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-16 14:36:22 +0700

    Create data/bikes.py

---
+++ data/bikes.py
@@ -0,0 +1,12 @@
+BIKES = {
+    "697.76": {"model": "Yamaha Janus"},
+    "141.56": {"model": "Honda Lead"},
+    "100.22": {"model": "Honda Lead"},
+    "776.43": {"model": "Honda Lead"},
+    "493.12": {"model": "Yamaha NVX"},
+    "316.44": {"model": "Honda Vision 1"},
+    "216.58": {"model": "Honda Vision 2"},
+    "154.76": {"model": "Honda Airblade"},
+    "581.32": {"model": "Honda PCX"},
+    "NN409": {"model": "Honda PCX"},
+}
